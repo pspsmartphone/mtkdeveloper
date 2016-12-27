10e6b37dd9ad3a5020e01944f28077a1d7df4b09
pspsmartphone <pspsmartphone@gmail.com>
2016-12-28 06:59:58 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="go-to-url_27.html"}
+if(x=="1"){xxx="go-to-url_64.html"}
 if(x=="2"){xxx="mtkdeveloper-download-files.html"}
 if(x=="3"){xxx="download-file.html"}
 if(x=="4"){xxx="waiting.html"}a_to_ve[i].href="https://mtkdeveloperfile.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
