68c2fa65884aec5a72c770f79cb7859a302daa44
MTKDeveloper <pspsmartphone@gmail.com>
2017-09-21 11:41:19 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="10-reasons-why-android-os-is-better.html"}
 if(x=="2"){xxx="5-reasons-to-creat-mobile-app-for-your.html"}
 if(x=="3"){xxx="7-benefits-of-playing-video-games.html"}
-if(x=="4"){xxx="5-dynamic-ways-to-get-your-iphone-app.html"}a_to_ve[i].href="https://mtkdeveloperfile.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="5-dynamic-ways-to-get-your-iphone-app.html"}a_to_ve[i].href="http://www.mtkdeveloperfile.xyz/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
