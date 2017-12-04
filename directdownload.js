5c8dab842fc46232caebcb777c18888e8376afd2
MTKDeveloper <pspsmartphone@gmail.com>
2017-12-04 13:30:10 +0700

    Add files via upload

---
+++ directdownload.js
@@ -0,0 +1,8 @@
+<!-- Safelink -->
+<script src='https://rawgit.com/pspsmartphone/mtkdeveloper/master/safelinkv2_utama.js' type='text/javascript'/>
+<script type='text/javascript'>
+//<![CDATA[
+protected_links = "go.sumo.com,lazada.co.id,mtkdeveloper.com,linkedin.com,twitter.com,youtube.com,facebook.com"; //Simpan domain yang tidak ingin di-encrypted 
+auto_safelink();
+//]]>
+</script>
